e2cccd083077c219aef61cd199ca4b378911b80b
LamDatTuong <vkjb8kqm6x@privaterelay.appleid.com>
2026-01-27 23:02:10 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -12,4 +12,22 @@ app.use(express.json());
 const contactRoutes = require("./app/routes/contact.route");
 app.use("/api/contacts", contactRoutes);
 
+const ApiError = require("./app/api-error");
+
+// handle 404 response
+app.use((req, res, next) => {
+  //code ở đây sẽ chạy khi không tìm thấy route
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  //middleware xử lý lỗi tập trung
+  // Trong các đoạn code xử ở các route nếu gọi next(err) thì sẽ chuyển về middleware xử lý lỗi này
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
